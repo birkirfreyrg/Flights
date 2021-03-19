57ec72003fa23ee549108f4463ea5fdef1d0def5
birkirfreyrg <32616114+birkirfreyrg@users.noreply.github.com>
2021-03-19 17:46:46 +0000

    Búa til verkefni
    Bæta öllum skrám inn!

---
+++ Testy/src/Flight/Search.java
@@ -0,0 +1,9 @@
+package Flight;
+
+public class Search {
+
+	public static void main(String[] args) {
+		System.out.println("Virkar??");
+		
+	}
+}
---
+++ Testy/src/Flight/SearchTest.java
@@ -0,0 +1,27 @@
+package Flight;
+
+import org.junit.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchTest {
+
+	@Before
+	public void setUp() {
+		fail("Not yet implemented");
+	}
+	
+	@After
+	public void tearDown() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSomeBehavior() {
+		fail("Not yet implemented");
+		
+		//assert ();
+	}
+
+}
