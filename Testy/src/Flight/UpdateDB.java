4d6a311d055986a0c59eba0e3ee71cba59f043df
magnusingason <80974014+magnusingason@users.noreply.github.com>
2021-03-20 17:01:46 +0000

    reyndi updateDB classan

---
+++ Testy/src/Flight/UpdateDB.java
@@ -0,0 +1,36 @@
+package Flight;
+
+import java.sql.*;
+
+public class UpdateDB {
+	public static void insertIntoDB(String[] userInfo) throws SQLException {
+		Connection con = null;
+		try {
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:flightsdb.db");
+		System.out.println("connected");
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e+"");
+		}
+		Statement st = con.createStatement();
+		String sqlCreatingTable = "CREATE TABLE IF NOT EXISTS User(id INTEGER PRIMARY KEY, name varchar(30), email varchar(30))";
+		st.execute(sqlCreatingTable);
+		Statement st1 = con.createStatement();
+		String sqlInsertingValues = "INSERT INTO User(name, email) VALUES(" + userInfo[0] + ", " + userInfo[1] + " )";
+		st1.execute(sqlInsertingValues);
+		
+		String checking = "SELECT * FROM User";
+		Statement st2 = con.createStatement();
+		System.out.println(st2.execute(checking));
+		
+		
+	}
+	
+	public static void main(String[] args) throws SQLException{
+		String[] userInfo = new String[2];
+		userInfo[0] = "Indiana Jones";
+		userInfo[1] = "jones123@gmail.com";
+		insertIntoDB(userInfo);
+	}
+	
+}
