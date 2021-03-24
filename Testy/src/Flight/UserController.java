3793ddeacc9c9779d8148f9d79c33ee81ee6933b
birkirfreyrg <birkirfrey@msn.com>
2021-03-24 15:21:50 +0000

    Changed Controller
    User controller

--- Testy/src/Flight/UserController.java
+++ Testy/src/Flight/UserController.java
@@ -6,14 +6,18 @@ public class UserController extends User{
 	
 	private static User user = new User();
 	
-	public void createUser(String nm, String em) {
+	public static UserMock createUser(String nm, String em) {
 		String[] userinfo = {nm, em};
+		int id = -1;
 		try {
-			UpdateDB.insertIntoDB(userinfo);
+			id = UpdateDB.insertIntoDB(userinfo);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		UserMock myUser = new UserMock(nm, em, id);
+		System.out.println(nm+" | "+ em + " | " + id);
+		return myUser;
 	}
 	
 	public void updateUser() {
@@ -26,8 +30,7 @@ public class UserController extends User{
 	
 
 	public static void main(String[] args) {
-		user.setName("foo bar");
-		System.out.println(user.getName());
+		createUser("nameTest", "emailTest"); 
 	}
 
 }
--- Testy/src/Flight/UserMock.java
+++ Testy/src/Flight/UserMock.java
@@ -4,15 +4,21 @@ public class UserMock {
 	
 	private String name;
 	private String email;
+	private int id;
 	
-	public UserMock(String name, String email) {
+	public UserMock(String name, String email, int id) {
 		this.name = name;
 		this.email = email;
+		this.id = id;
 	}
 	
 	public void setName(String name) {
         this.name = name;
     }
+	
+	public int getId() {
+		return this.id;
+	}
 	
 	public String getName() {
 		return this.name;
@@ -27,7 +33,7 @@ public class UserMock {
 	}
 
 	public static void main(String[] args) {
-		UserMock um = new UserMock("Johnny", "Johnny@gmail.com");
+		UserMock um = new UserMock("Johnny", "Johnny@gmail.com", 1);
 		System.out.println(um.getName() + " | " + um.getEmail());
 	}
 
