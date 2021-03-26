6d4a657c7090d6caa049c2e27f9ba3dfdfc1b146
birkirfreyrg <birkirfrey@msn.com>
2021-03-26 16:34:17 +0000

    Update FlightsMock.java

--- Testy/src/Flight/FlightsMock.java
+++ Testy/src/Flight/FlightsMock.java
@@ -1,16 +1,24 @@
 package Flight;
-import java.util.ArrayList;
+import java.util.*;
 
 public class FlightsMock {
 	
 	// returns 5x flights
-	public static Flight getFlights() {
-		
+	public static  ArrayList<Flight> getFlights() {
+		ArrayList<Flight> flightList = new ArrayList<Flight>();
+		flightList.add();
+		flightList.add();
+		flightList.add();
 		return flightList;	
 	}
 
 	public static void main(String[] args) {
-		
+		ArrayList<Flight> flightList = new ArrayList<Flight>();
+		  Flight f1 = new Flight("Reykjavík", "Akureyri", );    
+		flightList.add();
+		for (int i = 0; i < flightList.size(); i++) {
+		    System.out.println(flightList.get(i));
+		}
 	}
 
 }
