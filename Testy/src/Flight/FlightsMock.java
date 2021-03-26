a04712c4e9baa331e11242d53b55626be0afd3c1
birkirfreyrg <birkirfrey@msn.com>
2021-03-26 16:19:11 +0000

    Create FlightsMock.java

---
+++ Testy/src/Flight/FlightsMock.java
@@ -0,0 +1,16 @@
+package Flight;
+import java.util.ArrayList;
+
+public class FlightsMock {
+	
+	// returns 5x flights
+	public static Flight getFlights() {
+		
+		return flightList;	
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
