de75c2e1af36922b81d85f557f935857a547c273
birkirfreyrg <birkirfrey@msn.com>
2021-03-26 17:23:01 +0000

    Update FlightsMock.java

--- Testy/src/Flight/FlightsMock.java
+++ Testy/src/Flight/FlightsMock.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class FlightsMock {
 	
+	
 	// returns 2x flights
 	public static  ArrayList<Flight> getFlights() {
 		Date date = new Date();
@@ -17,14 +18,12 @@ public class FlightsMock {
 	}
 
 	public static void main(String[] args) {
-		Date date = new Date();
-		ArrayList<Flight> flightList = new ArrayList<Flight>();
-		  Flight f1 = new Flight("Reykjavík", "Akureyri", date); 
-		  Flight f2 = new Flight("Akureyri","Reykjavík", date);
-		flightList.add(f1);
-		flightList.add(f2);
-		for (int i = 0; i < flightList.size(); i++) {
-		    System.out.println(flightList.get(i));
-		}
+		ArrayList<Flight> flightList = getFlights();
+           for(int i=0;i<flightList.size();i++)  
+           {  
+            System.out.println(flightList.get(i).getDestination());
+            System.out.println(flightList.get(i).getCurrentLocation()); 
+            System.out.println(flightList.get(i).getDepartureTime()); 
+           }  
 	}
 }
