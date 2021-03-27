d9ede248e3e29808fa08e8ddccfb1c0e682cac26
birkirfreyrg <birkirfrey@msn.com>
2021-03-27 12:50:45 +0000

    Update FlightsMock.java

--- Testy/src/Flight/FlightsMock.java
+++ Testy/src/Flight/FlightsMock.java
@@ -1,4 +1,6 @@
 package Flight;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -6,18 +8,20 @@ public class FlightsMock {
 	
 	
 	// returns 2x flights
-	public static  List<Flight> getFlights() {
-		Date date = new Date();
+	public static  List<Flight> getFlights() throws ParseException {
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
 		ArrayList<Flight> flightList = new ArrayList<Flight>();
-		Flight f1 = new Flight("Reykjavík", "Akureyri", date); 
-		Flight f2 = new Flight("Akureyri","Reykjavík", date);
+		Date d1 = sdformat.parse("2021-03-27");
+		Date d2 = sdformat.parse("2021-04-10");
+		Flight f1 = new Flight("Reykjavík", "Akureyri", d1); 
+		Flight f2 = new Flight("Akureyri","Reykjavík", d2);
 		flightList.add(f1);
 		flightList.add(f2);
 		return flightList;
 		
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		List<Flight> flightList = getFlights();
            for(int i=0;i<flightList.size();i++)  
            {  
