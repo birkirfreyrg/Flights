093aa1aa2c78ddbe7e4d73b3b5d2167adf4d5dee
Birgitta Ýr <birgittaye@gmail.com>
2021-04-09 16:48:22 +0000

    FlightController created

---
+++ Testy/src/Flight/FlightController.java
@@ -0,0 +1,9 @@
+package Flight;
+
+
+public class FlightController  {
+	private Flight[] flights;
+	
+	public Flight[] getFlights() { return flights; }
+	public void setFlights(Flight[] newflights) { flights = newflights; }
+}
