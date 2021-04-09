95626dc48e5c795ef6f154d514a7c17b61d3fef6
birkirfreyrg <birkirfrey@msn.com>
2021-04-09 16:52:05 +0000

    Update LocationController.java

--- Testy/src/Flight/LocationController.java
+++ Testy/src/Flight/LocationController.java
@@ -1,10 +1,13 @@
 package Flight;
 
+import java.util.*;
+
 public class LocationController {
+	
+	private List<Location> locations;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
