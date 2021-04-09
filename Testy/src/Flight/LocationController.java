cc0d536069b2508698a668fb0e117c79479a299c
birkirfreyrg <birkirfrey@msn.com>
2021-04-09 17:16:33 +0000

    Update LocationController.java

--- Testy/src/Flight/LocationController.java
+++ Testy/src/Flight/LocationController.java
@@ -5,8 +5,17 @@ import java.util.*;
 public class LocationController {
 	
 	private List<Location> locations;
+	
+	public List<Location> getLocations() {
+		return this.locations;
+	}
+	
+	LocationController(List<Location> newLocations) {
+		locations = newLocations;
+	}
 
 	public static void main(String[] args) {
+		LocationController fakeData = new locations
 		
 	}
 
