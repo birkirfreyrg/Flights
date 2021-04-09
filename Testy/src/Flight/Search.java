3aed4e66fc9f7b99fc4c06a23b9ec042a45da5a8
gislira <gra6@hi.is>
2021-04-09 17:06:47 +0000

    validatesearch flipp
    sear

--- Testy/src/Flight/Search.java
+++ Testy/src/Flight/Search.java
@@ -4,8 +4,12 @@ import java.util.*;
 
 public class Search {
 	
-	public static boolean validateText(String searchText) {
-		// asdasd 
+	public static boolean validateText(String searchText) {	
+		int x = (searchText.length());
+		if ( (x==0)||(x>30) )  {
+			return false;
+		}
+		return true;
 	}
 	
 	public static List<Flight> returnMatches(String search) {
@@ -29,7 +33,8 @@ public class Search {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Virkar??");
+		boolean thepriceisright = validateText("virkar þessi strengur?");
+		System.out.println(thepriceisright);
 		
 	}
 }
