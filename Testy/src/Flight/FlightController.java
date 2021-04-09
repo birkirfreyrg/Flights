1d2ce1d8c68a9600a20c36438a6cc5e69ea22c4c
Birgitta Ýr <birgittaye@gmail.com>
2021-04-09 16:55:04 +0000

    update FlighController

--- Testy/src/Flight/FlightController.java
+++ Testy/src/Flight/FlightController.java
@@ -4,6 +4,19 @@ package Flight;
 public class FlightController  {
 	private Flight[] flights;
 	
-	public Flight[] getFlights() { return flights; }
+	public Flight[] getFlights() { return flights; } // á að returna list<Flight> ?
 	public void setFlights(Flight[] newflights) { flights = newflights; }
+	
+	public void createFlight() { // accepts args[]
+		// TODO: create function
+		/*
+		Flight flight = new Flight(args);
+		flights.append(Flight);
+		*/
+		
+	}
+	
+	public void cancelFlight(int id) {
+		// TODO: create function
+	}
 }
