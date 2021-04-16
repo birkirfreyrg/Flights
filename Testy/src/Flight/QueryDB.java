badaa4f18c8e2127466a7a0b615f9990336646ec
birkirfreyrg <birkirfrey@msn.com>
2021-04-16 17:20:22 +0000

    Search as T wants

--- Testy/src/Flight/QueryDB.java
+++ Testy/src/Flight/QueryDB.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.Date;
@@ -94,13 +95,75 @@ public class QueryDB {
         return flightList;
         
     }
+	
+	public static List<Flight> selectFromDB(String destination, String currentLoc, Date dateFrom, Date dateTo) throws SQLException, ParseException {
+        Connection con = null;
+        List<Flight> flightList = new ArrayList<Flight>();
+        try {
+        Class.forName("org.sqlite.JDBC");
+        con = DriverManager.getConnection("jdbc:sqlite:flightsdb.db");
+        Statement st2 = con.createStatement();
+        String scanning = "SELECT * FROM Flight WHERE destination = '"+ destination +"' AND currentLoc = '"+ currentLoc + "'";
+        ResultSet rs = st2.executeQuery(scanning);
+        if (!rs.isBeforeFirst() ) {    
+            System.out.println("Could not find flight"); 
+        } 
+        while(rs.next()) {
+            String departureTime = rs.getString("departureTime");
+            String arrivalTime = rs.getString("arrivalTime");
+            Date DT = (Date) new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.ENGLISH).parse(departureTime);
+            Date AT = (Date) new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.ENGLISH).parse(arrivalTime);
+
+            if(!DT.before(dateFrom) && !DT.after(dateTo)) {
+            	Flight f = new Flight(rs.getString("destination"),rs.getString("currentLoc"), DT, AT);
+                f.setID(rs.getInt("id"));
+                flightList.add(f);
+            }
+        }
+        rs.close();
+        
+        
+        }catch (ClassNotFoundException | SQLException e) {
+            System.out.println("ClassNotFound & SQL Exception; "+e);
+        } finally
+        {
+            try
+              {
+                if(con != null)
+                  con.close();
+              }
+              catch(SQLException e)
+              {
+                // connection close failed.
+                System.err.println("error closing database; "+e);
+              }
+        }    
+        return flightList;
+        
+    }
 
 public static void main(String[] args) throws SQLException, ParseException{
 	
+	Date date = new Date();
+	Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    calendar.add(Calendar.DAY_OF_MONTH, -4);
+    calendar.add(Calendar.HOUR_OF_DAY, -17);
+    Date dateTo = new Date();
+    dateTo = calendar.getTime();
+    calendar.setTime(date);
+    calendar.add(calendar.DAY_OF_MONTH, -6);
+    Date dateFrom = new Date();
+    dateFrom = calendar.getTime();
+    
+    System.out.println(dateFrom);
+    System.out.println(dateTo);
+	
+	
 	List<Flight> flightList = new ArrayList<Flight>();
-	flightList = selectFromDB("Reykjavik", "Akureyri");
-	Flight f = flightList.get(0);
-	System.out.print(f.toString());
+	flightList = selectFromDB("Reykjavik", "Akureyri", dateFrom, dateTo);
+	System.out.print(flightList);
+	
 }
 
 }
