ac0001632264e0b00763bda814f950f52dabdba9
birkirfreyrg <birkirfrey@msn.com>
2021-04-16 18:02:57 +0000

    Update InitializeFlightDB.java

--- Testy/src/Flight/InitializeFlightDB.java
+++ Testy/src/Flight/InitializeFlightDB.java
@@ -9,16 +9,25 @@ import java.util.Locale;
 
 public class InitializeFlightDB {
 	
-	 public static Date parseDate(String date) {
-	     try {
-	         return new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.ENGLISH).parse(date);
-	     } catch (ParseException e) {
-	         return null;
-	     }
-	  }
-	 
-
+	public static Date parseDate(String date) {
+		try {
+			return new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.ENGLISH).parse(date);
+			} catch (ParseException e) {
+				return null;
+				}
+		}
 	
+	public void initializeFlightDB() throws SQLException {
+		UpdateDB.deletFlightDB();
+		InitializeFlightDB ifdb = new InitializeFlightDB();
+		ifdb.createFlightInDB("Ólafsvík", "Reykjavík", "Fri Apr 16 20:00:00 GMT 2021");
+		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 16 22:00:00 GMT 2021");
+		ifdb.createFlightInDB("Olafsvik", "Reykjavik", "Fri Apr 17 20:00:00 GMT 2021");
+		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 17 22:00:00 GMT 2021");
+		ifdb.createFlightInDB("Olafsvik", "Reykjavik", "Fri Apr 18 20:00:00 GMT 2021");
+		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 18 22:00:00 GMT 2021");
+	}
+	 
 	public void createFlightInDB(String dest, String currLoc, String departureDate) throws SQLException {
 		Date date = parseDate(departureDate);
 		Calendar calendar = Calendar.getInstance();
@@ -30,14 +39,7 @@ public class InitializeFlightDB {
 	}
 
 	public static void main(String[] args) throws SQLException {
-		UpdateDB.deletFlightDB();
-		InitializeFlightDB ifdb = new InitializeFlightDB();
-		ifdb.createFlightInDB("Ólafsvík", "Reykjavík", "Fri Apr 16 20:00:00 GMT 2021");
-		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 16 22:00:00 GMT 2021");
-		ifdb.createFlightInDB("Olafsvik", "Reykjavik", "Fri Apr 17 20:00:00 GMT 2021");
-		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 17 22:00:00 GMT 2021");
-		ifdb.createFlightInDB("Olafsvik", "Reykjavik", "Fri Apr 18 20:00:00 GMT 2021");
-		ifdb.createFlightInDB("Reykjavík", "Ólafsvík", "Fri Apr 18 22:00:00 GMT 2021");
+	
 	}
 
 }
